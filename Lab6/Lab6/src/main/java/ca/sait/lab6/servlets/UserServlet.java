8ab340ddcd23dd37ec4c4e129bef15f5621d45c5
Jiasheng Lu <1473875731@qq.com>
2022-03-11 20:13:36 -0700

    Add servlets

---
+++ Lab6/Lab6/src/main/java/ca/sait/lab6/servlets/UserServlet.java
@@ -0,0 +1,59 @@
+package ca.sait.lab6.servlets;
+
+import ca.sait.lab6.modules.User;
+import ca.sait.lab6.services.UserService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jason
+ */
+public class UserServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserService service = new UserService();
+
+        try {
+            List<User> users = service.getAll();
+            
+            request.setAttribute("users", users);
+            this.getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
