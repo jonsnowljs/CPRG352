0627f81df6e2e420de1cd8340648a80c5ffa3f6d
Jiasheng Lu <1473875731@qq.com>
2022-03-11 18:57:36 -0700

    Add data access layer

--- Lab6/Lab6/src/main/java/ca/sait/lab6/modules/Role.java
+++ Lab6/Lab6/src/main/java/ca/sait/lab6/modules/Role.java
@@ -8,9 +8,36 @@ package ca.sait.lab6.modules;
 import java.io.Serializable;
 
 /**
- *
+ * Represents a role
  * @author Jason
  */
 public class Role implements Serializable{
+    private int id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
